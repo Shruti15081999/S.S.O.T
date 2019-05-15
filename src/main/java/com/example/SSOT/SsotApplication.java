33a01a5a832ebef4b7ccd880d54eddb721f09070
Shruti Joshi <shrutijoshi360@gmail.com>
2019-05-15 16:00:29 +0530

    initial commit

---
+++ src/main/java/com/example/SSOT/SsotApplication.java
@@ -0,0 +1,13 @@
+package com.example.SSOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SSOT/SsotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.SSOT;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
